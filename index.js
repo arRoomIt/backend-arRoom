c33434c99c08860a0bd028772ce3ae1cc0031e60
arRoomIt <arroom.it@gmail.com>
2021-09-07 15:38:40 +0200

    FEAT: added index.js to try db conection and working

---
+++ index.js
@@ -0,0 +1,43 @@
+//imports paquetes
+const express = require('express');
+
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+//importando archivos
+const db = require('./utils/db');
+db.connect();
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const router = express.Router();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/',router);
+
+
+router.get('/',(req,res) => {
+
+    res.status(200).json("backend funcionando!!!!");
+});
+
+
+//para controlar paginas que no existe
+app.use('*',(req,res,next) => {
+    const error = new Error('Página no encontrada');
+    return res.status(404).json(error.message);
+})
+
+app.use((error,req,res,next) => {
+    return res.status(error.status || 500).json(error.message || 'Unexpected error');
+})
+
+
+app.listen(PORT,() => {
+    console.log(`Server runnig in http://localhost:${PORT}`);
+});

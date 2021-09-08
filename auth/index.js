c6a2f3e1c4a5b7479304142397fe5319870082cd
Mohibul <tuhin28012@gmail.com>
2021-09-08 15:08:51 +0200

    FIX: fixed userId in index.js of auth

--- auth/index.js
+++ auth/index.js
@@ -1,13 +1,13 @@
 import passport from "passport";
 import registerStrategy from "./register.strategy";
-import loginStrategy from "./login.strategy";
+// import loginStrategy from "./login.strategy";
 import User from "../models/User.model";
 
 passport.serializeUser((user,done)=>{
     return done(null,user._id);
 });
 
-passport.deserializeUser(async(user,done)=>{
+passport.deserializeUser(async(userId,done)=>{
 
     try {
         const existingUser = await User.findById(userId);
@@ -18,8 +18,9 @@ passport.deserializeUser(async(user,done)=>{
 });
 
 const setStrategies = () => {
+    console.log("setStrategies")
     passport.use("register", registerStrategy);
-    passport.use("login", loginStrategy);
+    //passport.use("login", loginStrategy);
 }
 
 export default setStrategies;

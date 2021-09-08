35c2a1f1da259057567ba1a7438ac0c552d8c714
Javier9614 <javierarandalopez96@gmail.com>
2021-09-08 10:04:52 +0200

    MERGE: crud of review, and models (User,Review) from branch javi

---
+++ models/User.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema ( 
+    { 
+        email:{type: String , required: true},
+        password:{type: String , required: true},
+        name:{type: String , required: true},
+        phoneNumber:{type: String , required: true},
+        role:{type: String , required: true, default: "user", enum: ['user', 'admin'] },
+        isHost:{type: Boolean , required:true},
+        profileImage:{type: String },
+        reviews:[{type: mongoose.Types.ObjectId, ref:"Review"}],
+        reservations:[{type: mongoose.Types.ObjectId, ref:"Reservation"}]
+    },
+        {timestamps:true}
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;

67d908b47bdcc2bdc4cf33a30216dcfc2dd07be4
Juan José Trigo Hormigos <trigo.hormigos@bootcamp-upgrade.com>
2021-09-10 19:19:18 +0200

    FEAT: create node mailer, it works

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,7 @@
 import User from '../models/User.model';
+import nodemailer from 'nodemailer';
+
+
 
 
 const getUser = async (req, res, next) => {
@@ -14,6 +17,56 @@ const getUser = async (req, res, next) => {
     } catch (error) {
         return next(error);
     }
+};
+
+const sendEmail = async (req, res, next) => {
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.MAILUSER,
+            pass: process.env.MAILPSSWD
+        }
+    });
+
+    let mail_options = {
+        from: 'ArRoomMJT',
+        to: 'arroom.it@gmail.com',
+        subject: 'Bienvenido a la aplicación',
+        html: `
+            <table border="0" cellpadding="0" cellspacing="0" width="600px" background-color="#2d3436" bgcolor="#2d3436">
+                <tr height="200px">  
+                    <td bgcolor="" width="600px">
+                        <h1 style="color: #fff; text-align:center">Bienvenido</h1>
+                        <p  style="color: #fff; text-align:center">
+                            <span style="color: #e84393"></span> 
+                            a la aplicación
+                        </p>
+                    </td>
+                </tr>
+                <tr bgcolor="#fff">
+                    <td style="text-align:center">
+                        <p style="color: #000">¡Un mundo de workspaces a su disposición!</p>
+                    </td>
+                </tr>
+            </table>
+        `
+    };
+
+    try {
+        transporter.sendMail(mail_options, (error, info) => {
+            if(error) {
+                return res.status(500).json(error.message);
+            }else{
+                return res.status(200).json('El correo se envió correctamente');
+            }
+        });
+        
+    } catch (error) {
+        return next(error);
+    }
+
+   
 };
 
 
@@ -66,4 +119,5 @@ const deleteUser = async (req, res, next) => {
 
 export { getUser,
          deleteUser,
-         editUser } 
+         editUser,
+         sendEmail} 
--- routes/User.routes.js
+++ routes/User.routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { getUser, deleteUser, editUser } from '../controllers/user.controller';
+import { getUser, deleteUser, editUser, sendEmail } from '../controllers/user.controller';
+
 
 const routerUser = express.Router();
 
@@ -7,6 +8,8 @@ const routerUser = express.Router();
 routerUser.get('/', getUser);
 
 routerUser.put('/edit', editUser);
+
+routerUser.post('/send-email', sendEmail);
 
 routerUser.delete('/delete', deleteUser);
 

bd0c7a7384eb7b4e82e056c070ecc133c4b0c7f3
Juan José Trigo Hormigos <trigo.hormigos@bootcamp-upgrade.com>
2021-09-11 09:04:06 +0200

    FEAT: creation of utils.User.js file to move the html for sending the mail

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/User.model';
 import nodemailer from 'nodemailer';
+import mail_options from '../utils/utils.User';
 
 
 
@@ -19,6 +20,8 @@ const getUser = async (req, res, next) => {
     }
 };
 
+
+
 const sendEmail = async (req, res, next) => {
 
     let transporter = nodemailer.createTransport({
@@ -28,30 +31,6 @@ const sendEmail = async (req, res, next) => {
             pass: process.env.MAILPSSWD
         }
     });
-
-    let mail_options = {
-        from: 'ArRoomMJT',
-        to: 'arroom.it@gmail.com',
-        subject: 'Bienvenido a la aplicación',
-        html: `
-            <table border="0" cellpadding="0" cellspacing="0" width="600px" background-color="#2d3436" bgcolor="#2d3436">
-                <tr height="200px">  
-                    <td bgcolor="" width="600px">
-                        <h1 style="color: #fff; text-align:center">Bienvenido</h1>
-                        <p  style="color: #fff; text-align:center">
-                            <span style="color: #e84393"></span> 
-                            a la aplicación
-                        </p>
-                    </td>
-                </tr>
-                <tr bgcolor="#fff">
-                    <td style="text-align:center">
-                        <p style="color: #000">¡Un mundo de workspaces a su disposición!</p>
-                    </td>
-                </tr>
-            </table>
-        `
-    };
 
     try {
         transporter.sendMail(mail_options, (error, info) => {
@@ -65,7 +44,6 @@ const sendEmail = async (req, res, next) => {
     } catch (error) {
         return next(error);
     }
-
    
 };
 

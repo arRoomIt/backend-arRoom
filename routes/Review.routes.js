013be071ce542b91abf0cfd9c4ab7745156e972d
Mohibul <tuhin28012@gmail.com>
2021-09-11 13:08:41 +0200

    FEAT: added routes for user and workspace review

--- routes/Review.routes.js
+++ routes/Review.routes.js
@@ -1,14 +1,23 @@
  import express from 'express';
  import{isUser, isAdmin,isHost}from '../middlewares/auth.middleware';
- import {reviewGet,reviewCreate,reviewPut,reviewDelete,reviewGetById,reviewWorkspace} from "../controllers/review.controller";
+ import {
+    reviewGet,
+    reviewCreate,
+    reviewPut,
+    reviewDelete,
+    reviewGetById,
+    reviewUser,
+    reviewWorkspace } from "../controllers/review.controller";
 
  const routerReview = express.Router();
 
  routerReview.get('/',reviewGet);
+//TODO: get de los review de workspace y de user
 
-
- routerReview.post('/workspace',reviewWorkspace);
+ routerReview.post('/workspace',[isUser],[isAdmin],reviewWorkspace);
 //TODO: comentar sobre un usuario siendo un host
+
+ routerReview.post('/user',reviewUser);
 
  routerReview.post('/create',[isUser],[isAdmin],[isHost], reviewCreate);
 

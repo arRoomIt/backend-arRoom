ab8c0312c22b687676e9971c0d6626b72b62c674
Mohibul <tuhin28012@gmail.com>
2021-09-14 14:59:13 +0200

    FIX: changed line of console log in auth middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,14 +1,14 @@
 const isUser = (req, res, next) => {
-    console.log("isUser-->", req.user.role);
     if(req.isAuthenticated()){
+        console.log("isUser-->", req.user.role);
         return next()
     }
     return res.status(401).json("Registrate/logueate");
 };
 
 const isHost = (req, res, next) => {
-    console.log("isHost-->", req.user.role);
     if(req.isAuthenticated()){
+        console.log("isHost-->", req.user.role);
         if(req.user.role==="host"){
             return next();
         }
@@ -20,8 +20,8 @@ const isHost = (req, res, next) => {
 }
 
 const isAdmin = (error,req, res, next) => {
-    console.log("isAdmin-->", req.user.role);
     if(req.isAuthenticated()){
+        console.log("isAdmin-->", req.user.role);
         if(req.user.role==="admin"){
             return next();
         }
